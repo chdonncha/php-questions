22d17a96ef4626efd4ab63a3d77b7db8a470cb3d
donncha <chdonncha@gmail.com>
2022-05-09 22:52:29 +0100

    completed first question

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
 <?php
-    echo "Welcome to PHPSandbox";
+
+PrimeNumbers();
+
+function PrimeNumbers()
+{
+    $numbers = array_map(function ($values) {
+        return $values;
+    }, range(1, 100));
+
+    foreach ($numbers as &$number) {
+
+        $multiples = null;
+
+        // Get multiples
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % $number === 0) {
+                $multiples .= $i . ",";
+            }
+        }
+
+        $multiples = substr($multiples, 0, -1);
+
+        if ($multiples == $number) {
+            $number = $number . "[PRIME] \n";
+        } else {
+            $number = $number . " (" . $multiples . ") \n";
+        }
+    }
+
+    echo implode(' ', $numbers);
+}
